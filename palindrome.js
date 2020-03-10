75d39478b644b6d79a4f02c4fc9211cbc46f1e6f
AllenJacob <allenjc1995@gmail.com>
2020-03-10 06:14:18 +0530

    palindrom

---
+++ palindrome.js
@@ -0,0 +1,19 @@
+var x=122;
+var y=x;
+var rev=0;
+var rem=0;
+while(x>0)
+{
+   rem=x%10;
+   rev=rev*10+rem;
+   x=parseInt(x/10);
+  
+}
+if(rev==y)
+{
+    console.log("number is a palindrome")
+}
+else if(rev!=y)
+{
+    console.log("number is not a palindrome")
+}
